3d77783011e5b91ba66ae804661dbe5fea33ce50
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2022-10-24 20:30:17 +0300

    Domino Piling

--- domino_piling.py
+++ domino_piling.py
@@ -1,6 +1,4 @@
 def dominoPiling(m, n):
     board_squares = m * n
     max_no_of_dominoes = board_squares//2
-    return max_no_of_dominoes
-
-print(dominoPiling(7,3))
+    print(max_no_of_dominoes)
